91613a773150b07773a4630e85d1d0fa4d9815d5
Tanner Nielsen <tannerntannern@gmail.com>
2019-01-17 00:27:54 -0600

    Started integrating commander

---
+++ src/bin.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import * as program from 'commander';
+const packageJson = require('../package.json');
+
+program
+	.version(packageJson.version, '-v, --version');
+
+// TODO: THIS COMMAND SHOULD TOTALLY UTILIZE ITSELF
+program
+	.command('init <name>')
+	.description('Initializes a project scaffold')
+	.action((name, options) => {
+		// TODO:
+		console.log('Calling `init` with', name);
+	});
+
+program
+	.command('*');
+
+program.parse(process.argv);
