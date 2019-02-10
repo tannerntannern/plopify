a304a3b27b9650d95d673fcb15c9b291f072c8a9
Tanner Nielsen <tannerntannern@gmail.com>
2019-02-10 13:53:15 -0600

    feat: drafted the full gamut of commands

--- src/bin.ts
+++ src/bin.ts
@@ -1,29 +1,44 @@
 #!/usr/bin/env node
 import * as path from 'path';
 import * as program from 'commander';
-const packageJson = require('../package.json');
-
+import chalk from 'chalk';
 import loader from 'rc.ts';
+import {prompt} from 'inquirer';
+
+const packageJson = require('../package.json');
 import {RCSchema} from './rc.schema';
+
+const header = chalk.bold.bgBlue(' ' + packageJson.name + ' ') + chalk.bold.bgYellow(' v' + packageJson.version + ' ');
 
 program
 	.version(packageJson.version, '-v, --version');
 
-// TODO: THIS COMMAND SHOULD TOTALLY UTILIZE ITSELF
 program
-	.command('init <name>')
-	.description('Initializes a project scaffold')
-	.action((name, options) => {
-		// TODO:
-		console.log('Calling `init` with', name);
+	.command('gen <template> <outdir>')
+	.description('Generates a new project based on the given template')
+	.action(async (template: string, outdir: string, options) => {
+		console.log(header);
+
+		const config = loader(RCSchema).loadConfigFile(path.resolve(template, '.plopifyrc.js'));
+
+		const answers = await prompt(config.prompts);
+
+		console.log(answers);
 	});
 
 program
-	.command('read <name>')
-	.description('Temporary testing command for parsing a config')
-	.action((name, options) => {
-		const data = loader(RCSchema).loadConfigFile(path.resolve(name));
-		console.log(data);
+	.command('update <project>')
+	.description('Updates an existing project based on changes from the template')
+	.option('-p --prompts', 'prompt the user for input again instead of using the saved answers')
+	.action((template, output, options) => {
+		// TODO: ...
+	});
+
+program
+	.command('init <dir>')
+	.description('Initializes a project template that can be used to generate other projects')
+	.action((dir, options) => {
+		// TODO: ...
 	});
 
 program.parse(process.argv);
--- test/playground/template/.plopifyrc.js
+++ test/playground/template/.plopifyrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  templateVersion: "1.0.0",
   prompts: [{
-    name: 'test'
+    name: 'project_name',
+    message: 'Project name?',
+    validate: (input) => /^[a-z0-9\-]+$/.test(input) ? true : 'Must be a dash-case-name'
   }],
-  ignore: []
+  updatePolicies: []
 };
