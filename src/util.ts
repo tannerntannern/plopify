b8fe2f8e5b8df172974f90308ccaee8a14ca24ba
Tanner Nielsen <tannerntannern@gmail.com>
2019-02-12 12:19:07 -0600

    fix: extracted a few util functions

---
+++ src/util.ts
@@ -0,0 +1,35 @@
+/**
+ * Collection of utility functions.  If this file grows large enough, I'll just include lodash, but that doesn't seem
+ * necessary at the moment.
+ */
+import chalk from 'chalk';
+
+/**
+ * Filters out array duplicates.
+ */
+export function unique(array: any[]): any[] {
+	return array.filter((el, i, arr) => arr.indexOf(el) === i);
+}
+
+/**
+ * Computes the intersection of two arrays.
+ */
+export function intersection(array1: any[], array2: any[]): any[] {
+	return unique(
+		array1.filter(el => -1 !== array2.indexOf(el))
+	);
+}
+
+/**
+ * Computes the union of two arrays.
+ */
+export function union(array1: any[], array2: any[]): any[] {
+	return unique(array1.concat(array2));
+}
+
+/**
+ * Logs ✔/✘ for true/false
+ */
+export function logStatus(success: boolean) {
+	console.log(success ? chalk.greenBright('✔ ') : chalk.red('✘ '));
+}
