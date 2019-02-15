0e13fa36f7656de1484f47c91aa5fc1dd1bc84ce
Tanner Nielsen <tannerntannern@gmail.com>
2019-02-15 13:00:08 -0600

    test: finished the utility function test suite (for now)

--- test/unit/Util.test.ts
+++ test/unit/Util.test.ts
@@ -18,18 +18,70 @@ describe('Utility functions', function(){
 	});
 
 	describe('intersection()', function(){
-		// TODO: ...
+		it('should return an empty when either of the inputs are empty arrays', function(){
+			expect(intersection([], [])).to.deep.equal([]);
+			expect(intersection([1, 2, 3], [])).to.deep.equal([]);
+			expect(intersection([], [1, 2, 3])).to.deep.equal([]);
+		});
+
+		it('should return an empty array when there is no overlap', function(){
+			expect(intersection([1, 2, 3], [4, 5, 6])).to.deep.equal([]);
+		});
+
+		it('should return the intersection for the standard case', function(){
+			expect(intersection([1, 2, 3, 4], [3, 4, 5, 6])).to.deep.equal([3, 4]);
+		});
 	});
 
 	describe('union()', function(){
-		// TODO: ...
+		it('should return an empty array for empty-array inputs', function(){
+			expect(union([], [])).to.deep.equal([]);
+		});
+
+		it('should return the non-empty array when one of the inputs is an empty array', function(){
+			expect(union([1, 2, 3], [])).to.deep.equal([1, 2, 3]);
+			expect(union([], [1, 2, 3])).to.deep.equal([1, 2, 3]);
+		});
+
+		it('should return the union for the standard case', function(){
+			expect(union([1, 2, 3], [3, 4, 4, 5, 6])).to.deep.equal([1, 2, 3, 4, 5, 6]);
+		});
 	});
 
 	describe('difference()', function(){
-		// TODO: ...
+		it('should return an empty array for empty inputs', function(){
+			expect(difference([], [])).to.deep.equal([]);
+		});
+		
+		it('should return the minuend when the subtrahend is empty', function(){
+			expect(difference([1, 2, 3], [])).to.deep.equal([1, 2, 3]);
+		});
+
+		it('should return an empty array when the subtrahend is empty, regardless of the minuend', function(){
+			expect(difference([], [1, 2, 3])).to.deep.equal([]);
+			expect(difference([], [1])).to.deep.equal([]);
+		});
+
+		it('should return an empty array when both inputs are equal', function(){
+			expect(difference([1, 2, 3], [1, 2, 3])).to.deep.equal([]);
+		});
+
+		it('should return the difference for the standard case', function(){
+			expect(difference([1, 2, 3, 4], [2, 4])).to.deep.equal([1, 3]);
+		});
 	});
 
 	describe('arrayify()', function(){
-		// TODO: ...
+		it('should return an empty array for null input', function(){
+			expect(arrayify(null)).to.deep.equal([]);
+		});
+
+		it('should return an array with one element when a string is passed', function(){
+			expect(arrayify('string')).to.deep.equal(['string']);
+		});
+
+		it('should return the input if the input is an array', function(){
+			expect(arrayify(['a', 'b', 'c'])).to.deep.equal(['a', 'b', 'c']);
+		});
 	});
 });
--- test/unit/temp.test.ts
+++
@@ -1,8 +0,0 @@
-import 'mocha';
-import {expect} from 'chai';
-
-describe('Dumb placeholder test', function(){
-	it('should work', function(){
-		expect('test').to.equal('test');
-	});
-});
