bf74a065357085d94c141c4c6d5b7ec670112e05
Tanner Nielsen <tannerntannern@gmail.com>
2019-02-19 11:23:47 -0600

    fix: made the password prompts able to use a shared password cache

--- src/lib/prompts.ts
+++ src/lib/prompts.ts
@@ -1,4 +1,5 @@
 import {prompt} from 'inquirer';
+import * as bcrypt from 'bcrypt';
 
 /**
  * Quick, dirty utility to get confirmation from the user.
@@ -10,7 +11,29 @@ export const confirm = async (message: string): Promise<boolean> => (await promp
 }]) as any).confirmation;
 
 /**
- * Collects a confirmed password from the user, recursively restarting until they don't fuck it up.
+ * Gets a password from the user without confirmation using a custom message.
+ *
+ * @internal
+ * @see guessAndCheckPassword
+ */
+const getPassword = async (message: string = 'Please enter your password:'): Promise<string> => {
+	return (await prompt([{
+		type: 'password',
+		name: 'password',
+		mask: '*',
+		message: message
+	}]) as any).password;
+};
+
+/**
+ * A cache available by closure to collectPassword() and guessAndCheckPassword().  This prevents the user from ever
+ * being asked for their password more than once per session.
+ */
+let passwordCache = null;
+
+/**
+ * Collects a confirmed password from the user, recursively restarting until they don't fuck it up.  If another function
+ * subsequently needs the password, the password will be available in the passwordCache.
  */
 export const collectPassword = async (): Promise<string> => {
 	const passwordData: {password: string, password_confirm: string} = await prompt([{
@@ -29,18 +52,23 @@ export const collectPassword = async (): Promise<string> => {
 		console.log('The password confirmation does not match.  Please try again.');
 		return collectPassword();
 	} else {
+		passwordCache = passwordData.password;
 		return passwordData.password;
 	}
 };
 
 /**
- * Gets a password from the user without confirmation using a custom message.
+ * Allows the user to check their password against the given hash and keep retrying until they get it right.  Returns
+ * the correct password once it has been found.  The function also caches a copy of the correct password in memory, so
+ * that the user need not be asked for it multiple times per session.
  */
-export const getPassword = async (message: string = 'Please enter your password:'): Promise<string> => {
-	return (await prompt([{
-		type: 'password',
-		name: 'password',
-		mask: '*',
-		message: message
-	}]) as any).password;
+export const guessAndCheckPassword = async (passwordHash: string): Promise<string> => {
+	if (passwordCache !== null) return passwordCache;
+
+	passwordCache = await getPassword();
+	while (!await bcrypt.compare(passwordCache, passwordHash)) {
+		passwordCache = await getPassword('Incorrect password. Try again:');
+	}
+
+	return passwordCache;
 };
