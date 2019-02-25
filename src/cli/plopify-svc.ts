f2b13df4847323e649e62624b91682ea75ae56c2
Tanner Nielsen <tannerntannern@gmail.com>
2019-02-24 23:50:35 -0600

    feat: added `travis-enable` to `plopify svc`

--- src/cli/plopify-svc.ts
+++ src/cli/plopify-svc.ts
@@ -5,15 +5,14 @@ import axios from 'axios';
 import chalk from 'chalk';
 
 import globalConfig from '../lib/global-config';
-
-// Where the dist directory is from this file
-const distDir = path.resolve(__dirname, '..');
+import {logStatus} from '../util/misc';
+const conf = globalConfig(path.resolve(__dirname, '..'));
 
 program
 	.command('github-create <name>')
 	.option('-p --private', 'Whether to make the new repo private')
 	.action(async (name, options) => {
-		const token = await globalConfig(distDir).read('githubPersonalAccessToken');
+		const token = await conf.read('githubPersonalAccessToken');
 
 		try {
 			console.log('Attempting to create repository:', name);
@@ -31,8 +30,6 @@ program
 			}));
 
 			console.log('Successfully created', chalk.blue.underline(res.data.clone_url));
-
-			return;
 		} catch (e) {
 			if (e.response) {
 				process.stdout.write(chalk.bgRed.bold(' ' + e.response.status + ' ') + ' ');
@@ -43,7 +40,45 @@ program
 				}
 			}
 
-			throw e;
+			process.exit(1);
+		}
+	});
+
+program
+	.command('travis-enable <slug>')
+	.action(async (slug, options) => {
+		const config = await conf.read();
+		const user = config.githubUsername;
+		const token = config.travisCiApiToken;
+
+		const base = 'https://api.travis-ci.org';
+		const headers = {
+			'Travis-API-Version': 3,
+			'User-Agent': 'Plopify',
+			'Authorization': 'token ' + token
+		};
+
+		try {
+			process.stdout.write('Authorizing... ');
+			let userInfo = (await axios.get(`${base}/user`, {headers})).data;
+			logStatus(true);
+
+			process.stdout.write('Syncing Travis CI to GitHub... ');
+			await axios.post(`${base}/user/${userInfo.id}/sync`, {}, {headers});
+			logStatus(true);
+
+			process.stdout.write(`Enabling Travis CI builds for ${slug}... `);
+			await axios.post(`${base}/repo/${encodeURIComponent(slug)}/activate`, {}, {headers});
+			logStatus(true);
+		} catch (e) {
+			logStatus(false);
+
+			if (e.response) {
+				// TODO: research the response codes to deliver more meaningful error messages
+				console.log(e.response.data);
+			}
+
+			process.exit(1);
 		}
 	});
 
