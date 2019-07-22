5309d4f60c57857c0d0edbeb918269ae924f07cf
Tanner Nielsen <tannerntannern@gmail.com>
2019-07-21 21:00:58 -0500

    chore: deleted most of the dead code

--- src/api/svc/coverallsEnable.ts
+++
@@ -1,3 +0,0 @@
-export const coverallsEnable = () => {
-	// TODO: ...
-};
--- src/util/templates.ts
+++
@@ -1,8 +0,0 @@
-import * as hbs from 'handlebars';
-
-/**
- * Simple function that uses handlebars to render a template string with data.
- */
-export const renderString = (templateString: string, data: {[key: string]: any}): string => {
-	return hbs.compile(templateString)(data);
-};
